e19269cab50af9453b094b65c927717b29d403d1
Goularte G.C <92948398+ggoulartec@users.noreply.github.com>
2022-09-15 22:09:33 -0300

    (bairro, cidade, estado) - model - repository

--- InitializrSpringbootProject/src/main/java/com/example/demo/model/Estado.java
+++ InitializrSpringbootProject/src/main/java/com/example/demo/model/Estado.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.demo.model;
 
-/**
- *
- * @author gusta
- */
 public class Estado {
-    
+
+    private Integer codEstado;
+    private String nome;
+    private String uf;
+    private String pais;
+
+    public Estado() {
+    }
+
+    public Estado(Integer codEstado, String nome, String uf, String pais) {
+        this.codEstado = codEstado;
+        this.nome = nome;
+        this.uf = uf;
+        this.pais = pais;
+    }
+
+    public Integer getCodEstado() {
+        return codEstado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    @Override
+    public String toString() {
+        return "Estado{" + "codEstado=" + codEstado + ", nome=" + nome + ", uf=" + uf + ", pais=" + pais + '}';
+    }
+
 }
---
+++ InitializrSpringbootProject/src/main/java/com/example/demo/repository/BairroRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Bairro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BairroRepository extends JpaRepository<Bairro, Integer> {
+
+}
