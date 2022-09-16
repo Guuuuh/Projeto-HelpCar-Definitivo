7dd11ea7941469de643ba5bd76143cbfe9c324d6
gabrielvitoretti <gabriel.vitoretti@outlook.com>
2022-09-15 22:25:29 -0300

    Alterações realizadas nas classes Cliente e Veículo.

---
+++ InitializrSpringbootProject/src/main/java/com/example/demo/model/Cliente.java
@@ -0,0 +1,92 @@
+
+package com.example.demo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author gabrielvitoretti
+ */
+public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codcliente")
+    private Integer codCliente;
+    
+    @Column(name = "nome")
+    private String nome;
+    
+    @Column(name = "cpf")
+    private String cpf;
+    
+    private List<Endereco> enderecoCliente = new ArrayList<>();
+    
+    @OneToMany
+    @JoinColumn(name = "codcliente")
+    private List<Veiculo> veiculos = new ArrayList<>();
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer codCliente, String nome, String cpf) {
+        this.codCliente = codCliente;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public Integer getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(Integer codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public List<Endereco> getEnderecoCliente() {
+        return enderecoCliente;
+    }
+
+    public void setEnderecoCliente(List<Endereco> enderecoCliente) {
+        this.enderecoCliente = enderecoCliente;
+    }
+
+    public List<Veiculo> getVeiculos() {
+        return veiculos;
+    }
+
+    public void setVeiculos(List<Veiculo> veiculos) {
+        this.veiculos = veiculos;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "codCliente=" + codCliente + ", nome=" + nome + ", cpf=" + cpf + ", enderecoCliente=" + enderecoCliente + ", veiculos=" + veiculos + '}';
+    }
+    
+    
+    
+}
--- InitializrSpringbootProject/src/main/java/com/example/demo/model/Veiculo.java
+++ InitializrSpringbootProject/src/main/java/com/example/demo/model/Veiculo.java
@@ -1,12 +1,15 @@
 
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 /**
@@ -20,12 +23,13 @@ public class Veiculo implements Serializable{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    
     @Column(name = "codveiculo")
-    private int codVeiculo;
+    private Integer codVeiculo;
     
-    @Column(name = "codcliente")
-    private int codCliente;
+    @ManyToOne
+    @JoinColumn(name = "codcliente")
+    @JsonIgnore
+    private Cliente cliente;
     
     @Column(name = "marca")
     private String marca; 
@@ -42,49 +46,57 @@ public class Veiculo implements Serializable{
     public Veiculo() {
     }
 
-    public Veiculo(int codVeiculo, int codCliente, String marca, String modelo, String ano, String placa) {
+    public Veiculo(Integer codVeiculo, Cliente cliente, String marca, String modelo, String ano, String placa) {
         this.codVeiculo = codVeiculo;
-        this.codCliente = codCliente;
+        this.cliente = cliente;
         this.marca = marca;
         this.modelo = modelo;
         this.ano = ano;
         this.placa = placa;
     }
 
-    public int getCodVeiculo() {
+    public Integer getCodVeiculo() {
         return codVeiculo;
     }
 
-    public int getCodCliente() {
-        return codCliente;
+    public void setCodVeiculo(Integer codVeiculo) {
+        this.codVeiculo = codVeiculo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
     }
 
     public String getMarca() {
         return marca;
     }
 
-    public String getModelo() {
-        return modelo;
-    }
-
-    public String getAno() {
-        return ano;
-    }
-
-    public String getPlaca() {
-        return placa;
-    }
-
     public void setMarca(String marca) {
         this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
     }
 
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
+    public String getAno() {
+        return ano;
+    }
+
     public void setAno(String ano) {
         this.ano = ano;
+    }
+
+    public String getPlaca() {
+        return placa;
     }
 
     public void setPlaca(String placa) {
@@ -93,6 +105,11 @@ public class Veiculo implements Serializable{
 
     @Override
     public String toString() {
-        return "Veiculo{" + "codVeiculo=" + codVeiculo + ", codCliente=" + codCliente + ", marca=" + marca + ", modelo=" + modelo + ", ano=" + ano + ", placa=" + placa + '}';
+        return "Veiculo{" + "codVeiculo=" + codVeiculo + ", cliente=" + cliente + ", marca=" + marca + ", modelo=" + modelo + ", ano=" + ano + ", placa=" + placa + '}';
     }
+    
+    
+    
+    
+    
 }
