1e6ed6a50b5b858ce156ddc969ec1641a2f66ed2
Goularte G.C <92948398+ggoulartec@users.noreply.github.com>
2022-09-15 22:23:41 -0300

    Update Estado.java

--- InitializrSpringbootProject/src/main/java/com/example/demo/model/Estado.java
+++ InitializrSpringbootProject/src/main/java/com/example/demo/model/Estado.java
@@ -1,53 +1,72 @@
 package com.example.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "estado")
 public class Estado {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codestado")
     private Integer codEstado;
+
+    @Column(name = "nome")
     private String nome;
-    private String uf;
-    private String pais;
 
-    public Estado() {
+    public class Estado {
+
+        private Integer codEstado;
+        private String nome;
+        private String uf;
+        private String pais;
+
+        public Estado() {
+        }
+
+        public Estado(Integer codEstado, String nome, String uf, String pais) {
+            this.codEstado = codEstado;
+            this.nome = nome;
+            this.uf = uf;
+            this.pais = pais;
+        }
+
+        public Integer getCodEstado() {
+            return codEstado;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public String getUf() {
+            return uf;
+        }
+
+        public String getPais() {
+            return pais;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public void setUf(String uf) {
+            this.uf = uf;
+        }
+
+        public void setPais(String pais) {
+            this.pais = pais;
+        }
+
+        @Override
+        public String toString() {
+            return "Estado{" + "codEstado=" + codEstado + ", nome=" + nome + ", uf=" + uf + ", pais=" + pais + '}';
+        }
+
     }
-
-    public Estado(Integer codEstado, String nome, String uf, String pais) {
-        this.codEstado = codEstado;
-        this.nome = nome;
-        this.uf = uf;
-        this.pais = pais;
-    }
-
-    public Integer getCodEstado() {
-        return codEstado;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getUf() {
-        return uf;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setUf(String uf) {
-        this.uf = uf;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
-    @Override
-    public String toString() {
-        return "Estado{" + "codEstado=" + codEstado + ", nome=" + nome + ", uf=" + uf + ", pais=" + pais + '}';
-    }
-
-}
